0bef77a6b1b5a4badaf6e5c3ab02a85868a5001d
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2025-07-26 14:37:49 +0200

    20250726-1437 - setting up the terraform repository

--- internal/provider_test.go
+++ internal/provider_test.go
@@ -3,20 +3,7 @@ package provider
 import (
 	"os"
 	"testing"
-
-	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
-	"github.com/hashicorp/terraform-plugin-testing/echoprovider"
 )
-
-var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
-	"xtrm-fabric-engine": providerserver.NewProtocol6WithError(New("test")()),
-}
-
-var testAccProtoV6ProviderFactoriesWithEcho = map[string]func() (tfprotov6.ProviderServer, error){
-	"xtrm-fabric-engine": providerserver.NewProtocol6WithError(New("test")()),
-	"echo":               echoprovider.NewProviderServer(),
-}
 
 func testAccPreCheck(t *testing.T) {
 	if v := os.Getenv("EXTRM_FE_HOST"); v == "" {
