5a1c475cddb314604cde7325e821fac1ab19855a
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2025-07-26 15:09:56 +0200

    20250726-1509 - setting up the terraform repository

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -3,8 +3,9 @@ package provider
 import (
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	provider2 "github.com/tchevalleraud/extrm-fabric-engine/internal"
 )
 
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
-	"xtrm-fabric-engine": providerserver.NewProtocol6WithError(New("test")()),
+	"xtrm-fabric-engine": providerserver.NewProtocol6WithError(provider2.New("test")()),
 }
