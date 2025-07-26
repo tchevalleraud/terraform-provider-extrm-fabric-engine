df7e301b5b4fea6a443a15599657c1b9bb79e438
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2025-07-26 14:40:07 +0200

    20250726-1440 - setting up the terraform repository

--- internal/provider_test.go
+++ internal/provider_test.go
@@ -1,18 +1,3 @@
 package provider
 
-import (
-	"os"
-	"testing"
-)
-
-func testAccPreCheck(t *testing.T) {
-	if v := os.Getenv("EXTRM_FE_HOST"); v == "" {
-		t.Fatal("EXTRM_FE_HOST env variable must be set for acceptance tests")
-	}
-	if v := os.Getenv("EXTRM_FE_USERNAME"); v == "" {
-		t.Fatal("EXTRM_FE_USERNAME env variable must be set for acceptance tests")
-	}
-	if v := os.Getenv("EXTRM_FE_PASSWORD"); v == "" {
-		t.Fatal("EXTRM_FE_PASSWORD env variable must be set for acceptance tests")
-	}
-}
+import ()
